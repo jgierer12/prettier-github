df4b100599a6e855a737bb7648a406a2eca8576e
Haroen Viaene <fingebimus@me.com>
2017-06-11 17:11:47 +0200

    Add jsx language to format

--- format-comment.js
+++ format-comment.js
@@ -11,7 +11,7 @@ module.exports = commentRaw => {
 	const format = source => prettier.format(source, JSON.parse(process.env.PRETTIER_OPTIONS || '{}')).replace(/\n$/, '');
 
 	comment.blocks.map(block => {
-		if (JSON.parse(process.env.LANGS || '["js", "javascript", "es6", "css", "less", "scss", "ts"]').includes(block.lang)) {
+		if (JSON.parse(process.env.LANGS || '["js", "javascript", "jsx", "es6", "css", "less", "scss", "ts"]').includes(block.lang)) {
 			try {
 				block.code = format(block.code);
 			} catch (error) {
