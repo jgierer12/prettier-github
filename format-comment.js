1a4f6c7081207d780d7ddffea2dae3e5cecfa561
Jonas Gierer <jgierer12@outlook.com>
2017-06-13 16:45:01 +0200

    Add notice comment to formatted code blocks (#4)

--- format-comment.js
+++ format-comment.js
@@ -1,6 +1,14 @@
 const extract = require('extract-gfm');
 const prettier = require('prettier');
 
+const noticeComment = originalCode =>
+`<!-- The following code block was formatted with \`prettier\`. If this is not desired, please change this comment to \`prettier-github disable\`. A copy of your original code block is included below in case you want to restore it.
+Learn more about \`prettier-github\` at https://github.com/jgierer12/prettier-github
+
+${originalCode}
+
+-->`;
+
 module.exports = commentRaw => {
 	const comment = extract.parseBlocks(commentRaw);
 
@@ -10,16 +18,24 @@ module.exports = commentRaw => {
 
 	const format = source => prettier.format(source, JSON.parse(process.env.PRETTIER_OPTIONS || '{}')).replace(/\n$/, '');
 
-	comment.blocks.map(block => {
+	comment.blocks.forEach((block, i) => {
 		if (JSON.parse(process.env.LANGS || '["js", "javascript", "jsx", "es6", "css", "less", "scss", "ts"]').includes(block.lang)) {
 			try {
-				block.code = format(block.code);
+				const formattedCode = format(block.code);
+				if (formattedCode !== block.code) {
+					const newText = noticeComment(block.code) + '\n' + block.block.replace(block.code, formattedCode);
+					const noticeCommentRegex = noticeComment('__OLD_BLOCK__').replace(/([.*+?^=!:${}()|[\]/\\])/g, '\\$1');
+					const oldTextRegex = new RegExp(`(${noticeCommentRegex.replace('__OLD_BLOCK__', '[\\s\\S]*')}\n)?${extract.id(i)}`.replace(/\n/g, '(\n|(\r\n))'));
+					comment.text = comment.text.replace(oldTextRegex, newText);
+					return;
+				}
 			} catch (error) {
 				console.warn('Error while formatting code block:', error.message);
 			}
 		}
-		return block;
+
+		comment.text = comment.text.replace(extract.id(i), block.block);
 	});
 
-	return extract.injectBlocks(comment.text, comment.blocks);
+	return comment.text;
 };
--- format-comment.test.js
+++ format-comment.test.js
@@ -1,13 +1,19 @@
 const formatComment = require('./format-comment.js');
 
 test('Formats a code block', () => {
-	const comment = '```js\nfoo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());\n```\n';
+	const comment = '```js\nfoo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());\n```';
 
 	expect(formatComment(comment)).toMatchSnapshot();
 });
 
 test('Formats multiple code blocks', () => {
-	const comment = '```js\nfoo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());\n```\n\n```js\nfoo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());\n```\n';
+	const comment = '```js\nfoo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());\n```\n```js\nfoo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());\n```';
+
+	expect(formatComment(comment)).toMatchSnapshot();
+});
+
+test('Overrides notice if the comment has been formatted previously', () => {
+	const comment = '<!-- The following code block was formatted with `prettier`. If this is not desired, please change this comment to `prettier-github disable`. A copy of your original code block is included below in case you want to restore it.\nLearn more about `prettier-github` at https://github.com/jgierer12/prettier-github\n\nfoo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());\n\n-->\n```js\nbar()\n```';
 
 	expect(formatComment(comment)).toMatchSnapshot();
 });
@@ -19,19 +25,19 @@ test('Ignores no code blocks', () => {
 });
 
 test('Ignores incompatible code blocks', () => {
-	const comment = '```sh\necho "Nothing to format here"\n```\n';
+	const comment = '```sh\necho "Nothing to format here"\n```';
 
 	expect(formatComment(comment)).toBe(comment);
 });
 
 test('Ignores correctly formatted code blocks', () => {
-	const comment = '```js\nconst all = "good";\n```\n';
+	const comment = '```js\nconst all = "good";\n```';
 
 	expect(formatComment(comment)).toBe(comment);
 });
 
 test('Ignores comments containing `<!-- prettier-github disable -->`', () => {
-	const comment = '<!-- prettier-github disable -->\n\n```js\nfoo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());\n```\n';
+	const comment = '<!-- prettier-github disable -->\n\n```js\nfoo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());\n```';
 
 	expect(formatComment(comment)).toBe(comment);
 });
@@ -44,14 +50,14 @@ describe('invalid code blocks', () => {
 	});
 
 	test('Warns about invalid code blocks', () => {
-		const comment = '```js\ninvalid javascript\n```\n';
+		const comment = '```js\ninvalid javascript\n```';
 		formatComment(comment);
 
 		expect(global.console.warn).toHaveBeenCalled();
 	});
 
 	test('Ignores invalid code blocks', () => {
-		const comment = '```js\ninvalid javascript\n```\n';
+		const comment = '```js\ninvalid javascript\n```';
 
 		expect(formatComment(comment)).toBe(comment);
 	});
