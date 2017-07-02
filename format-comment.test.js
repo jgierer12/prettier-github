5c365911742f09c3576a94aa93649180143c4928
Jonas Gierer <jgierer12@outlook.com>
2017-07-02 11:31:17 +0200

    Test languages

--- format-comment.test.js
+++ format-comment.test.js
@@ -1,9 +1,35 @@
 const formatComment = require('./format-comment.js');
 
-test('Formats a code block', () => {
-	const comment = '```js\nfoo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());\n```';
+describe('languages', () => {
+	test('Formats a JavaScript code block', () => {
+		const comment = '```js\nfoo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());\n```';
 
-	expect(formatComment(comment)).toMatchSnapshot();
+		expect(formatComment(comment)).toMatchSnapshot();
+	});
+
+	test('Formats a CSS code block', () => {
+		const comment = '```css\n.foo {color: red;}\n```';
+
+		expect(formatComment(comment)).toMatchSnapshot();
+	});
+
+	test('Formats a TypeScript code block', () => {
+		const comment = '```ts\ninterface ITypeScript {supported: boolean}\n```';
+
+		expect(formatComment(comment)).toMatchSnapshot();
+	});
+
+	test('Formats a JSON code block', () => {
+		const comment = '```json\n{"supportJSON": true}\n```';
+
+		expect(formatComment(comment)).toMatchSnapshot();
+	});
+
+	test('Formats a GraphQL code block', () => {
+		const comment = '```graphql\n{language(name: "GraphQL") {isSupported}}\n```';
+
+		expect(formatComment(comment)).toMatchSnapshot();
+	});
 });
 
 test('Formats multiple code blocks', () => {
