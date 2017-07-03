c03417cf5f94a205f0656dc765fda083924065d5
Jonas Gierer <jgierer12@outlook.com>
2017-07-03 21:28:44 +0200

    Rework tests

--- format-comment.test.js
+++ format-comment.test.js
@@ -1,90 +1,125 @@
-const formatComment = require('./format-comment.js');
-
-describe('languages', () => {
-	test('Formats a JavaScript code block', () => {
-		const comment = '```js\nfoo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());\n```';
-
-		expect(formatComment(comment)).toMatchSnapshot();
-	});
-
-	test('Formats a CSS code block', () => {
-		const comment = '```css\n.foo {color: red;}\n```';
-
-		expect(formatComment(comment)).toMatchSnapshot();
-	});
-
-	test('Formats a TypeScript code block', () => {
-		const comment = '```ts\ninterface ITypeScript {supported: boolean}\n```';
-
-		expect(formatComment(comment)).toMatchSnapshot();
-	});
-
-	test('Formats a JSON code block', () => {
-		const comment = '```json\n{"supportJSON": true}\n```';
-
-		expect(formatComment(comment)).toMatchSnapshot();
-	});
-
-	test('Formats a GraphQL code block', () => {
-		const comment = '```graphql\n{language(name: "GraphQL") {isSupported}}\n```';
-
-		expect(formatComment(comment)).toMatchSnapshot();
-	});
+// const formatComment = require('./format-comment.js');
+beforeEach(() => {
+	jest.resetModules();
 });
 
-test('Formats multiple code blocks', () => {
-	const comment = '```js\nfoo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());\n```\n```js\nfoo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());\n```';
+test('Runs Prettier', () => {
+	const comment = '```js\n__CODE__\n```';
 
-	expect(formatComment(comment)).toMatchSnapshot();
+	const format = jest.fn().mockReturnValue('');
+	jest.doMock('prettier', () => ({format}));
+	const formatComment = require('./format-comment.js');
+
+	formatComment(comment);
+	expect(format).toHaveBeenCalled();
 });
 
-test('Overrides notice if the comment has been formatted previously', () => {
-	const comment = '<!-- The following code block was formatted with Prettier. If this is not desired, please change this comment to `prettier-github disable`. A copy of your original code block is included below in case you want to restore it.\nLearn more about Prettier GitHub` at https://github.com/jgierer12/prettier-github\n\nfoo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());\n\n-->\n```js\nbar()\n```';
+test('Passes code block to Prettier', () => {
+	const comment = '```js\n__CODE__\n```';
 
-	expect(formatComment(comment)).toMatchSnapshot();
+	const format = jest.fn().mockReturnValue('');
+	jest.doMock('prettier', () => ({format}));
+	const formatComment = require('./format-comment.js');
+
+	formatComment(comment);
+	expect(format.mock.calls[0][0]).toBe('__CODE__');
 });
 
-test('Ignores no code blocks', () => {
-	const comment = 'Nothing to format here\n';
+test('Passes language to Prettier via `filepath`', () => {
+	const comment = '```js\n__CODE__\n```';
 
-	expect(formatComment(comment)).toBe(comment);
+	const format = jest.fn().mockReturnValue('');
+	jest.doMock('prettier', () => ({format}));
+	const formatComment = require('./format-comment.js');
+
+	formatComment(comment);
+	expect(format.mock.calls[0][1].filepath).toBe('.js');
+});
+
+test('Outputs formatted code', () => {
+	const comment = '```js\n__CODE__\n```';
+
+	const format = jest.fn().mockReturnValue('__CODE_1__');
+	jest.doMock('prettier', () => ({format}));
+	const formatComment = require('./format-comment.js');
+
+	expect(formatComment(comment)).toMatch(/```js\n__CODE_1__[\s\S]*\n```$/);
+});
+
+test('Trims trailing newline', () => {
+	const comment = '```js\n__CODE__\n```';
+
+	const format = jest.fn().mockReturnValue('__CODE_1__\n');
+	jest.doMock('prettier', () => ({format}));
+	const formatComment = require('./format-comment.js');
+
+	expect(formatComment(comment)).toMatch(/```js\n__CODE_1__\n```$/);
+});
+
+test('Outputs notice comment', () => {
+	const comment = '```js\n__CODE__\n```';
+
+	const format = jest.fn().mockReturnValue('');
+	jest.doMock('prettier', () => ({format}));
+	const formatComment = require('./format-comment.js');
+
+	expect(formatComment(comment)).toMatch(/^<!--[\s\S]*-->/);
+});
+
+test('Outputs old code in notice comment', () => {
+	const comment = '```js\n__CODE__\n```';
+
+	const format = jest.fn().mockReturnValue('');
+	jest.doMock('prettier', () => ({format}));
+	const formatComment = require('./format-comment.js');
+
+	expect(formatComment(comment)).toMatch(/^<!--[\s\S]*__CODE__[\s\S]*-->/);
+});
+
+test('Overrides existing notice comments', () => {
+	const comment = '```js\n__CODE__\n```';
+
+	const format = jest.fn().mockReturnValueOnce('__CODE_1__').mockReturnValue('');
+	jest.doMock('prettier', () => ({format}));
+	const formatComment = require('./format-comment.js');
+
+	const processedComment = formatComment(formatComment(comment));
+	expect(processedComment).toMatch(/^<!--[\s\S]*__CODE_1__[\s\S]*-->/);
+	expect(processedComment).not.toMatch(/<!--[\s\S]*__CODE__[\s\S]*-->/);
+});
+
+test('Ignores comments without code blocks', () => {
+	const comment = 'stuff and things';
+
+	const format = jest.fn().mockReturnValue('');
+	jest.doMock('prettier', () => ({format}));
+	const formatComment = require('./format-comment.js');
+
+	const processedComment = formatComment(comment);
+	expect(format).not.toHaveBeenCalled();
+	expect(processedComment).toBe(comment);
 });
 
 test('Ignores incompatible code blocks', () => {
-	const comment = '```sh\necho "Nothing to format here"\n```';
+	const comment = '```sh\n__CODE__\n```';
 
-	expect(formatComment(comment)).toBe(comment);
-});
+	const format = jest.fn().mockReturnValue('');
+	jest.doMock('prettier', () => ({format}));
+	const formatComment = require('./format-comment.js');
 
-test('Ignores correctly formatted code blocks', () => {
-	const comment = '```js\nconst all = "good";\n```';
-
-	expect(formatComment(comment)).toBe(comment);
+	const processedComment = formatComment(comment);
+	expect(format).not.toHaveBeenCalled();
+	expect(processedComment).toBe(comment);
 });
 
 test('Ignores comments containing `<!-- prettier-github disable -->`', () => {
-	const comment = '<!-- prettier-github disable -->\n\n```js\nfoo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());\n```';
+	const comment = '<!-- prettier-github disable -->\n```js\n__CODE__\n```';
 
-	expect(formatComment(comment)).toBe(comment);
-});
+	const format = jest.fn().mockReturnValue('');
+	jest.doMock('prettier', () => ({format}));
+	const formatComment = require('./format-comment.js');
 
-describe('invalid code blocks', () => {
-	beforeEach(() => {
-		global.console = {
-			warn: jest.fn()
-		};
-	});
-
-	test('Warns about invalid code blocks', () => {
-		const comment = '```js\ninvalid javascript\n```';
-		formatComment(comment);
-
-		expect(global.console.warn).toHaveBeenCalled();
-	});
-
-	test('Ignores invalid code blocks', () => {
-		const comment = '```js\ninvalid javascript\n```';
-
-		expect(formatComment(comment)).toBe(comment);
-	});
+	const processedComment = formatComment(comment);
+	expect(format).not.toHaveBeenCalled();
+	expect(processedComment).toBe(comment);
 });
